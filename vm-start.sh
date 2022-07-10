169c5e175aaddce32decad06a43c10199f8582d0
HelloZhing <47844348+HelloZhing@users.noreply.github.com>
2022-07-10 14:36:30 +0800

    Add files via upload

--- vm-start.sh
+++ vm-start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 VMID="$1"
+igd_id="8086 $(lspci -n|grep '0:02.0'|cut -d ':' -f4|cut -c 1-4)"
 
 echo "VM $VMID is starting" > $(dirname $0)/$VMID-hooks.log
 
@@ -8,7 +9,8 @@ echo "VM $VMID is starting" > $(dirname $0)/$VMID-hooks.log
 
 sleep 1
 
-#echo 0000:00:02.0 > /sys/bus/pci/devices/0000\:00\:02.0/driver/unbind
-#echo 0000:00:1f.3 > /sys/bus/pci/devices/0000\:00\:1f.3/driver/unbind
-#echo 0000:00:02.0 > /sys/bus/pci/drivers/vfio-pci/bind
-#echo 0000:00:1f.3 > /sys/bus/pci/drivers/vfio-pci/bind
+echo 0000:00:02.0 > /sys/bus/pci/drivers/i915/unbind
+if ! lsmod | grep "vfio_pci" &> /dev/null ; then
+    modprobe vfio-pci
+fi
+echo $igd_id > /sys/bus/pci/drivers/vfio-pci/new_id
--- vm-stop.sh
+++ vm-stop.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 VMID="$1"
+ia_addr="0000:$(lspci|grep 'Audio'|grep 'Intel'|cut -c 1-7)"
+usb_addr="0000:$(lspci|grep 'USB'|grep 'Intel'|cut -c 1-7)"
+igd_id="8086 $(lspci -n|grep '0:02.0'|cut -d ':' -f4|cut -c 1-4)"
 
 echo "waitting" >> $(dirname $0)/$VMID-hooks.log
 
 sleep 10
 
 TimeSec=0
-
 until ! test -e "/var/run/qemu-server/$VMID.pid"
 do
     if [ $[$TimeSec%3600] -eq 0 ]; then
@@ -17,12 +19,13 @@ do
     let TimeSec+=3
 done
 
-echo 0000:00:02.0 > /sys/bus/pci/devices/0000\:00\:02.0/driver/unbind
-echo 0000:00:1f.3 > /sys/bus/pci/devices/0000\:00\:1f.3/driver/unbind
-echo 0000:00:14.0 > /sys/bus/pci/devices/0000\:00\:14.0/driver/unbind
+echo 0000:00:02.0 > /sys/bus/pci/drivers/vfio-pci/unbind
+echo $ia_addr > /sys/bus/pci/drivers/vfio-pci/unbind
+#echo $usb_addr > /sys/bus/pci/drivers/vfio-pci/unbind
+echo $igd_id > /sys/bus/pci/drivers/vfio-pci/remove_id
 echo 0000:00:02.0 > /sys/bus/pci/drivers/i915/bind
-echo 0000:00:1f.3 > /sys/bus/pci/drivers/snd_hda_intel/bind
-echo 0000:00:14.0 > /sys/bus/pci/drivers/xhci_hcd/bind
+echo $ia_addr > /sys/bus/pci/drivers/snd_hda_intel/bind
+#echo $usb_addr >/sys/bus/pci/drivers/xhci_hcd/bind
 
 sleep 1
 
